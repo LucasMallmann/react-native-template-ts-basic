27c6e9f299e3b30f5e3d42eae50a82ad008adefd
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-04-10 01:31:54 -0300

    feat: initial setup
    added eslint, typescript resolver & styled components

---
+++ src/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Greeting } from './styles';
+
+const Home = () => {
+  return (
+    <Container>
+      <Greeting>Hello World</Greeting>
+    </Container>
+  );
+};
+
+export default Home;
